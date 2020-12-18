3a4544ed43facc2c0eb015d04662cf68c0b3d230
richard <r50206v@gmail.com>
2020-12-18 15:59:00 -0500

    [FEATURE] adding pricing

--- package/pricing.py
+++ package/pricing.py
@@ -5,9 +5,28 @@ Linear pricing strategy: a linear equation of inventory level
 (e.g. price = -0.75*current_inventory + 30)
 '''
 
-def constant_price():
-    return 100000
+class Price(object):
+
+    def __init__(self, max_price=30):
+        self.max_price = max_price
 
 
-def linear_price():
-    pass
+    def constant_price(self, constant_price=None):
+        if constant_price:
+            return constant_price
+        else:
+            return self.max_price * 2/3
+
+
+    def linear_price(self, current_inventory, a=-0.25):
+        return a*current_inventory + self.max_price
+
+
+if __name__ == "__main__":
+    import numpy as np
+    price = Price(30)
+    print(price.constant_price(25))
+    print(price.constant_price())
+
+    current_inventory = np.arange(0, 120, 1)
+    print(price.linear_price(current_inventory))
